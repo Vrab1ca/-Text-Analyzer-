78d9abca7101e899e93696b408f0c2b4b85cc04f
Vrab1ca <niikoola08@gmail.com>
2025-03-04 23:33:35 +0200

    Add files via upload

---
+++ Text_Analyzer/Text_Analyzer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Text_Analyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
