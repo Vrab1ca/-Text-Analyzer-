c8bb67d12688ec8cafa83180673124b2271c7648
Vrab1ca <niikoola08@gmail.com>
2025-03-06 13:03:19 +0200

    Update -Text-Analyzer-.cs

--- Text_Analyzer/Text_Analyzer/Program.cs
+++ Text_Analyzer/Text_Analyzer/Program.cs
@@ -23,7 +23,19 @@ namespace Text_Analyzer
           var wordFrequency = GetWordFrequency(inputText , stopWords);
 
           Console.WriteLine($"\nСтатистика : ");
-          Console.Write
+          Console.WriteLins($"Брой думи: {wordCount}");
+          Console.WriteLine($"Брой символи: {charCount}");
+          Console.WriteLine("\nЧесто срещани думи: ");
+          foreach (var pair in weordFrequency.OrderByDescending(p => p.value).Take(10))
+            {
+              Console.WriteLins($"{pair.Key}: {pair.value} пъти");
         }
     }
+
+        static int CountWords(string text)
+        {
+            string[] words = text.Split(new[] { ' ', '\t' , '\n' , '\r' , '.' , 
+            ',' , '!' , '?', ';' , ':' , '(' , ')'),StringSplitOption.RemoveEmptyEntries);
+
+                    
 }
